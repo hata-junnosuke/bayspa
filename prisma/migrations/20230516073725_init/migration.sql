0f3a883b44f571bfb3b43a185a4b7912d9c61ac6
hata-junnosuke <jh19911209@icloud.com>
2023-05-16 16:53:04 +0900

    posgre導入

--- prisma/migrations/20230516073725_init/migration.sql
+++ prisma/migrations/20230516073725_init/migration.sql
@@ -1,19 +1,23 @@
 -- CreateTable
 CREATE TABLE "metadata" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "key" TEXT NOT NULL,
-    "value" TEXT NOT NULL
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "metadata_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "player" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "number" INTEGER NOT NULL,
     "fight_song" TEXT NOT NULL,
     "explanation" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "player_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
